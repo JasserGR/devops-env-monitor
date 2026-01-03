247613a622cc719262a770d4e98abdcc29191043
Jasser Gorsia <jassergorsia@gmail.com>
2026-01-03 12:16:16 +0100

    chore: initial project sturcture

---
+++ src/main/java/com/devops/project/envmonitor/EnvMonitorApplication.java
@@ -0,0 +1,11 @@
+package com.devops.project.envmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnvMonitorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/devops/project/envmonitor/EnvMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.project.envmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
